920a41a4f8c0cb02197c1a3bd183f781634ac3b4
SamyukthaMahesh <samyukthamahesh@gmail.com>
2016-02-16 22:21:22 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,7 @@
+number=int(input("ENTER YOUR number"))
+if number==0:
+    print ('the given number is a zero')
+elif number<0:
+    print ('the given number is negative')
+elif number>0:
+    print ('the given number is positive')
